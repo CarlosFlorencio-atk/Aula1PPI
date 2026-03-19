bdc56fa6b7c30414530d7012559efb16e0e5ebb9
Carlos Florencio <carlosed.florencio@gmail.com>
2026-03-19 18:37:51 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// npm init - iniciou o projeto
+// npm start - execulta o projeto
+// npm install express - para usar o express
+
+import express from 'express';
+
+const host = '0.0.0.0';
+const porta = 3000;
+
+const server = express(); //oferecendo ao desenvolvedor um servidor http de modo expresso
+
+//recheando o servidor com funcionalidades
+
+server.get('/', (requisicao, resposta) => {
+  resposta.send(`
+    <DOCTYPE htmk>
+    <html lang = "pt-br">
+    <head>
+        <meta charset = "UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Primeiro programa para internet usando node + express</title>
+    </head>
+    <body>
+        <h1> Primeiro programa para internet usando node + express</h1>
+        <h2> Olá, bem-vindo a página inicial</h2>
+    </body>
+    </html>
+    `);
+});
+
+server.get('/horaAtual', (requisicao, resposta) =>{
+    const horaAtual = new Date();
+    const hora = horaAtual.getHours() + ":" + horaAtual.getMinutes() + ":" + horaAtual.getSeconds();
+
+    resposta.send(`
+        <DOCTYPE htmk>
+    <html lang = "pt-br">
+    <head>
+        <meta charset = "UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Horário do Servidor </title>
+    </head>
+    <body>
+        <h1> Primeiro programa para internet usando node + express</h1>
+        <h2> Agora são ${hora}</h2>
+    </body>
+    </html>
+        `);
+
+});
+
+
+server.listen(porta, host, () => {
+    console.log(`Servidor escutando em http://${host}:${porta}`);
+
+});
